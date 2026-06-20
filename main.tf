05b172db76248f4347dd65a7936413ad733d1e71
potnurunarendragood <106417450+potnurunarendragood@users.noreply.github.com>
2026-06-20 11:22:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 3
   ami                    = "ami-0c78ef10ebf8c08db"
   instance_type          = "c7i-flex.large"
-  key_name               = "junks"
+  key_name               = "jenkins-server"
   vpc_security_group_ids = ["sg-02ff09ea9be40159e"]
   tags = {
     Name = var.instance_names[count.index]
